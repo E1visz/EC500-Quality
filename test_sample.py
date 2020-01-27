ebe80ba4aa2cf384612c4d064bdc86e7feec21cc
E1visz <55214592+E1visz@users.noreply.github.com>
2020-01-27 17:49:39 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -2,3 +2,17 @@ from ABtoRO import intToRoman
 
 def test_one():
     assert intToRoman(1) == "I"
+    assert intToRoman(2) == "II"
+    assert intToRoman(3) == "III"
+    assert intToRoman(50) == "L"
+    assert intToRoman(125) == "CXXV"
+    assert intToRoman(399) == "CCCXCIX"
+    assert intToRoman(678) == "DCLXXVIII"
+    assert intToRoman(1997) == "MCMXCVI"
+    assert intToRoman(2020) == "MMXX"
+
+def test_two():
+    assert intToRoman(-1) == 0
+    assert intToRoman(-2) == 0
+    assert intToRoman(-3) == 0
+    assert intToRoman(-50) == 0
